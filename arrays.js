27be20470df8026232297233d6152d740a6eecc6
Onur Eker <yonureker@gmail.com>
2017-11-01 21:49:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,3 +37,8 @@ function destructivelyRemoveElementFromEndOfArray (chocolateBars){
   chocolateBars.pop()
   return chocolateBars
 }
+
+function removeElementFromEndOfArray (chocolateBars){
+  chocolateBars.slice(0, chocolateBars.lenght - 1)
+  return chocolateBars
+}
